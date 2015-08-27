034423d7a26cd13117511e1e394bb2c1d562aa40
Lior <Lior@192.168.1.6>
2015-08-27 14:19:05 +0300

    zz

--- JavaNet_FinallProject/src/finallPro/model/Waiter.java
+++ JavaNet_FinallProject/src/finallPro/model/Waiter.java
@@ -1,6 +1,9 @@
 package finallPro.model;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -9,7 +12,9 @@ public class Waiter extends Thread{
 	private static Logger theLogger = Logger.getLogger("myLogger");
 	private Resturant theResturant;
 	private String name;
+	private boolean isInShift = true;
 	private Vector<Customer> allCustomers = new Vector<Customer>();
+	private Queue<Customer> waitingCustomers = new LinkedList<Customer>();
 	
 	public Waiter(String name, Resturant theResturant) {
 		super();
@@ -30,6 +35,14 @@ public class Waiter extends Thread{
 		}
 	}
 	
+	public String getTheName() {
+		return name;
+	}
+
+	public void setTheName(String name) {
+		this.name = name;
+	}
+
 	public Resturant getTheResturant() {
 		return theResturant;
 	}
@@ -42,42 +55,77 @@ public class Waiter extends Thread{
 		allCustomers.add(newCustomer);
 		newCustomer.start();
 	}
-
-	private void bringCheck() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.out.println("Waiter " + name + ": Going to bring the check");
-		Thread.sleep((int) (Math.floor(Math.random() * 10000) + 1000));
-	}
-
-	private void bringFood() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.out.println("Waiter " + name + ": Going to bring the food");
-		Thread.sleep((int) (Math.floor(Math.random() * 10000) + 1000));
-	}
-
-	private void takeOrder() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.out.println("Waiter " + name + ": Going to take the order");
-		Thread.sleep((int) (Math.floor(Math.random() * 10000) + 1000));
-	}
-
-	private void getMenu() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.out.println("Waiter " + name + ": Bring menu to customer");
-		Thread.sleep((int) (Math.floor(Math.random() * 10000) + 1000));
-	}
 	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		try {
-			getMenu();
-			takeOrder();
-			bringFood();
-			bringCheck();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void endShift() {
+		isInShift = false;
+		synchronized (/*dummyWaiter*/this) {
+			/*dummyWaiter.*/notifyAll();
 		}
+	}
+
+	public synchronized void addWaitingCustomer(Customer c) {
+		waitingCustomers.add(c);
+
+		System.out.println("After adding customer #" + c.getId()
+				+ " there are " + waitingCustomers.size()
+				+ " customers waiting");
+
+		synchronized (/*dummyWaiter*/this) {
+			if (waitingCustomers.size() == 1) {
+				/*dummyWaiter.*/notify(); // to let know there is an customer
+										// waiting
+			}
+		}
+	}
+
+	public synchronized void notifyCustomer() {
+		Customer firstCustomer = waitingCustomers.poll();
+		if (firstCustomer != null) {
+
+			System.out.println("Waiter is notifying customer #"
+					+ firstCustomer.getId());
+			synchronized (firstCustomer) {
+				firstCustomer.notifyAll();
+			}
+		}
+			try {
+
+				System.out.println("Waiter waits that customer #"
+						+ firstCustomer.getId()
+						+ " will announce it is finished");
+
+				wait(); // wait till the customer finishes
+
+				System.out.println("Waiter was announced that  customer #"
+						+ firstCustomer.getId() + " is finished");
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		//}
+	}
+
+	public void run() {
+		System.out.println("In Waiter::run");
+		while (isInShift) {
+			if (!waitingCustomers.isEmpty()) {
+				notifyCustomer();
+			} else {
+				synchronized (/*dummyWaiter*/this) {
+					try {
+						System.out.println("Waiter has no customers waiting");
+						/*dummyWaiter.*/wait(); // wait till there is an customer
+											// waiting
+						System.out
+								.println("Waiter recieved a message there is an customer waiting");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Shift ended for Waiter #" + getId() + this.name);
 	}
 }
